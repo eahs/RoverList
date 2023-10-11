fe097317d016a3623f0694798fdd879eadbd0853
Michael Tanczos <tanczosm@eastonsd.org>
2023-10-11 08:32:07 -0400

    Refresh to .net 7

---
+++ RoverList/RoverList/Program.cs
@@ -0,0 +1,24 @@
+﻿using RoverList;
+
+RoverList<string> list = new RoverList<string>();
+
+// TODO:  Implement the RoverList class
+// TODO:  Create a RoverList and then fill it with 16 words
+for (int i = 0; i < 16; i++)
+{
+    list.Add("apple" + i);
+}
+
+// TODO:  Print out the list
+
+// TODO:  Prompt the user to input words, add those words to the list until they enter the word "done"
+// TODO:  Print out the list
+
+// TODO:  Prompt the user to input words, add those words to the FRONT of the list until they enter the word "done"
+// TODO:  Print out the list
+
+// TODO:  Remove every word with an odd length
+// TODO:  Print out the list
+
+// TODO:  Clear the list
+// TODO:  Print out the list
---
+++ RoverList/RoverList/RoverList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RoverList;
+
+public class RoverList<T> : RoverListBase<T>
+{
+    public RoverList()
+    {
+        Count = 0;
+    }
+
+    public override int Count { get; }
+
+    public override void Add(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Add(int position, T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override T ElementAt(int position)
+    {
+        throw new NotImplementedException();
+    }
+
+    private Node NodeAt(int position)
+    {
+        // FIXME: Returning a forced null value
+        return null!;
+    }
+
+    public override void ListNodes()
+    {
+    }
+
+    public override bool RemoveAt(int position)
+    {
+        return false;
+    }
+}
